652ebdfcb1679fb28ae2be8cb310cb3bf5345f93
Roman Zakharov <roman.zaharov17@gmail.com>
2022-09-29 12:42:20 +0300

    fix: ios unsupported platform message (#31)

--- lib/src/base/exceptions.dart
+++ lib/src/base/exceptions.dart
@@ -12,5 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:io';
+
 /// Plugin doesn't support platform.
-class UnsupportedPlatform implements Exception {}
+class UnsupportedPlatform implements Exception {
+  @override
+  String toString() {
+    if (Platform.isIOS) {
+      return 'On iOS OTP autofill is built in TextField. Code from sms stores for 3 minutes.(see Readme)';
+    }
+    return super.toString();
+  }
+}
